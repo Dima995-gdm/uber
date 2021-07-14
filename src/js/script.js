7376530d177b3ae5cdc96e22482bd3c0a6a2a13d
Dima <gutenko.dim@yandex.ru>
2021-07-14 21:21:58 +0500

    add phpmailer and form

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,18 @@
 window.addEventListener('DOMContentLoaded', () => {
 	const menu = document.querySelector('.menu'),
-		menuItem = document.querySelectorAll('.menu_item'),
-		hamburger = document.querySelector('.hamburger');	
+		  menuItem = document.querySelectorAll('.menu_item'),
+		  hamburger = document.querySelector('.hamburger'),
+		  btnsModal = document.querySelectorAll('.subheader_btn, .promo_btn, #menu_request'),
+		  modal = document.querySelector('.overlay'),
+		  closeModal = document.querySelector('.modal__close'),
+		  form = document.querySelector('.feed-form');
+
+
+	toggleActiveModal = () => {
+		modal.classList.remove('active');
+		document.body.style.overflow = "";
+	}
+
 	hamburger.addEventListener('click', () => {
 		hamburger.classList.toggle('hamburger_active');
 		menu.classList.toggle('menu_active');
@@ -12,4 +23,65 @@ window.addEventListener('DOMContentLoaded', () => {
 			menu.classList.toggle('menu_active');
 		});
 	});
+	btnsModal.forEach((btn) => {
+		btn.addEventListener('click', () => {
+			modal.classList.add('active');
+			document.body.style.overflow = "hidden";
+		})
+	})
+	closeModal.addEventListener('click', toggleActiveModal)
+
+	window.addEventListener('click', (e) => {
+		if (e.target.classList.contains('active')) {
+			toggleActiveModal()
+		}
+	})
+
+	form.addEventListener('submit', (e) => {
+		e.preventDefault();
+
+		let formData = new FormData(form)
+
+		fetch('mailer/smart.php', {
+			method: 'POST',
+			body: formData,
+		})
+		.then (() => {
+			toggleActiveModal()
+			alert('Запрос отправлен');
+		})
+		.catch(() => {
+			alert('Произешел сбой при отправке данных');
+		})
+		.finally(() => {
+			form.reset();
+		})
+	});
+
+	// async function formSend(e) {
+	// 	e.preventDefault();
+
+	// 	let formData = new FormData(form)
+
+	// 	let response = await fetch('mailer/smart.php', {
+	// 		method: "POST",
+	// 		body: formData,
+	// 	})
+
+
+
+
+
+
+
+	// fetch('https://jsonplaceholder.typicode.com/posts', {
+	// 		method: "POST",
+	// 		body: JSON.stringify({name:'Dima'}),
+	// 		headers: {
+	// 			'Content-type': 'application/json'
+	// 		}
+	// 	})
+	// 	.then(response => response.json())
+	// 	.then(json => console.log(json))
+	
 });
